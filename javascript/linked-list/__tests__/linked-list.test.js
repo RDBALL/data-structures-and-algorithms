f8a324a7a5297ba976e9995be7327a7242140cde
Robert Ball <bobball89@gmail.com>
2022-09-24 01:21:11 +0000

    linked list code challenge functions and tests added

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,54 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+// Write tests to prove the following functionality:
+
+// Can successfully instantiate an empty linked list ✅
+// Can properly insert into the linked list ✅
+// The head property will properly point to the first node in the linked list ✅
+// Can properly insert multiple nodes into the linked list ✅
+// Will return true when finding a value within the linked list that exists ✅
+// Will return false when searching for a value in the linked list that does not exist ✅
+// Can properly return a collection of all the values that exist in the linked list ✅
+
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  let linkedListTest = new LinkedList;
+
+  test('Can successfully instantiate an empty linked list', () => {
+    expect(linkedListTest.head).toBeNull();
+  });
+
+  test('Can properly insert into the linked list', () => {
+    linkedListTest.insert(1);
+    expect(linkedListTest.head).toBeTruthy();
+  });
+
+  test('The head property will properly point to the first node in the linked list', () => {
+    expect(linkedListTest.head.value).toBe(1);
+    expect(linkedListTest.head.next).toBeNull;
+  });
+
+  test('Can property insert multiple nodes into the linked list', () => {
+    linkedListTest.insert(2);
+    linkedListTest.insert(3);
+    expect(linkedListTest.head.value).toBe(3);
+    expect(linkedListTest.head.next.value).toBe(2);
+  });
+
+  test('Will return true when finding a value within the linked list that exists', () => {
+    linkedListTest.insert(4);
+    expect(linkedListTest.includes(4)).toBe(true);
+  });
+
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    linkedListTest.insert(5);
+    expect(linkedListTest.includes(9999)).toBe(false);
+  });
+
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    // This const is reflecting the values added using linkedListTest.insert from the above tests
+    const collectionStringTest = `{${5}} -> {${4}} -> {${3}} -> {${2}} -> {${1}} -> NULL`;
+    expect(linkedListTest.toString()).toBe(collectionStringTest);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,66 @@
 'use strict';
 
-class LinkedList {
+// Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
+
+// Create a Linked List class
+// Within your Linked List class, include a head property.
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // The class should contain the following methods: insert, includes, to string
+
+  // ---------- insert ----------
+  // Arguments: value
+  // Returns: nothing
+  // Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+  insert(value) {
+    this.head = new Node(value, this.head);
+  }
+
+
+  // ---------- includes ----------
+  // Arguments: value
+  // Returns: Boolean
+  // Indicates whether that value exists as a Node’s value somewhere within the list.
+
+  includes(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (current.value === value) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  // ---------- to string ----------
+  // Arguments: none
+  // Returns: a string representing all the values in the Linked List, formatted as:
+  // "{ a } -> { b } -> { c } -> NULL"
+
+  toString() {
+    let current = this.head;
+    let string = '';
+    while (current !== null) {
+      string += `{${current.value}} -> `;
+      current = current.next;
+    }
+    return string + 'NULL';
+  }
+}
+
 
 module.exports = LinkedList;
